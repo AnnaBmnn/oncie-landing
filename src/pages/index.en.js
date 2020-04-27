7c5811e50c71e213ee93e25e8b1ebd7408483006
Marine LB <marine.leborgne@live.fr>
2020-04-27 11:06:05 +0200

    add english translation (except footer)

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -21,61 +21,62 @@ const IndexEnglishPage = () => (
     <SEO />
     <Header />
     <Hero
-      placeHolderTxt="EN Entrez votre email"
-      ctaTxt="EN Accédez à la béta"
-      title="EN anglais is audio qui propulse votre voix dans une nouvelle
-          dimension"
+      placeHolderTxt="Enter your email"
+      ctaTxt="Get early access"
+      title="The live audio app that pushes your voice to a new dimension"
     />
 
     <Section>
       <SectionHeader
         width="big"
         title=" EN Oncie permet à tous les podcasteur.ices d’offrir à leur communauté une nouvelle expérience audio "
-        titleHighlight="en air"
+        title="Oncie enables podcasters to offer a new experience for their communities through "
+        titleHighlight="live audio"
       />
       <BlocImgText
         imgSrc={img}
-        title="EN C'est à votre tour de prendre l'antenne"
-        description="EN Vous qui partagez vos passions, racontez des histoires, critiquez l’actualité, parlez de société, commentez le sport... Prenez l’antenne et offrez à votre communauté un show unique et interactif. "
+        title="It's your turn to be on air"
+        description="You, who shares your passions, tell stories, dissect the news, discuss society, comment on sports... Go on air and gift your audience with a unique and interactive show."
       />
     </Section>
     <BlocPhone
       imgSrc={phoneTop}
-      title="EN L’heure est venue de vous connecter avec votre audience"
-      description="EN Démarrez simplement votre diffusion audio...."
+      title="It's time to connect with your audience"
+      description="Simply start your live streaming...."
     ></BlocPhone>
     <Section>
       <SectionHeader
         width="small"
-        title="EN C'est partie, vous êtes en live"
+        title="Ready, set, go! You're on air"
         description="EN Oncie vous accompagne dans la création et la gestion de vos diffusions audio en direct, pour des interventions simplifiées et de qualité."
+        description="Oncie helps you create and manage your live audio streams, for simple and high-quality shows."
       />
       <Features
-        title1="EN La meilleure façon d’interagir avec votre communauté en temps réel"
-        description1="EN Oncie vous permet de recevoir les questions et commentaires les plus pertinents via un système de vote. Sondages, réactions, partage d’émotions : le live chat laisse la place aux témoignages de votre communauté en temps réel.  Rencontrez vos auditeur.ices et de renforcez le lien qui vous rassemble."
-        title2="EN L'opportunité d’animer des émission à plusieurs"
-        description2="Avez-vous déjà pensé à inviter une personne lors d’une émission ? Planifier un interview à distance ou diffuser un témoignage spontané d’une de vos auditeur.ices est desormais possible grâce à la fonctionnalité mutli-live."
-        title3="EN Libre à vous de cultiver un univers qui vous ressemble"
-        description3="Personnalisez et caractérisez vos émissions en ajoutant jingles et bandes son. Accédez aux données relatives à votre activité (nombre d’écoutes, interactions) pour comprendre votre audimat et optimiser l’impact de vos interventions."
+        title1="The best way to interact with your community in real time"
+        description1="Receive the most relevant questions and comments thanks to an upvote system. Polls, reactions, emotions sharing : the live chat lets your community participate in real time. Meet your listeners and find what brings you together."
+        title2="The chance to host a show together"
+        description2="Did you ever want to invite someone on your show&nbsp;? Planning a remote interview or letting a listener join you on air is now possible thanks to the multi-live feature."
+        title3="The freedom to make it your own"
+        description3="Customize and give character to your shows by adding jingles and music.  Get access to your shows data (number of listeners, interactions) to understand your audience and optimize the impact of your interventions."
       ></Features>
     </Section>
 
     <BlocPhone
       order="reverse"
       imgSrc={phoneBottom}
-      title="EN Votre travail est précieux, il doit être rémunéré."
-      description="EN Recherches, rédaction, interviews, animation, communication... La création de contenu audio de qualité est une activité complète et exigente. Votre contribution a une forte valeur ajoutée pour les auditeur.ice.s. C’est pourquoi, nous souhaitons que votre travail soit rémunéré."
+      title="Your work is valuable, it should be remunerated"
+      description="Research, writing, interviews, animation, communication... The creation of quality audio content is a complete and demanding activity. Your contribution has a lot of value for your listeners. This is why, we would like your work to be remunerated."
     ></BlocPhone>
-    <Insert title="EN Oncie fait vibrer votre audience."></Insert>
+    <Insert title="Oncie makes your audience vibrate"></Insert>
     <Section>
       <SectionHeader
         width="big"
-        title="Vous souhaitez tester en exclusivité la bêta ?"
-        description="Soyez le.a premier.e à utiliser Oncie avant sa sortie publique."
+        title="Would you like to get exclusive early access ?"
+        description="Be amongst the firsts to discover Oncie before the official release."
       />
       <CallToAction
-        placeHolderTxt="EN Entrez votre email"
-        ctaTxt="EN Accédez à la béta"
+        placeHolderTxt="Enter your email"
+        ctaTxt="Get early access"
       ></CallToAction>
     </Section>
   </Layout>
