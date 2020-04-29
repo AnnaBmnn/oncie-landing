8e4b5a8edb99049a2fc45542c8fff4b9b055097c
Anna <annabaumann@laposte.net>
2020-04-29 12:20:03 +0200

    animation line component

--- src/components/line/line.js
+++ src/components/line/line.js
@@ -1,20 +1,40 @@
 import React from "react"
-import GridRound from "../../inline/Grid_Round.inline.svg"
+import { useInView } from "react-intersection-observer"
 
 import lineStyles from "./line.module.scss"
 
 const Line = ({ isReverse }) => {
   const reverseClass = isReverse ? lineStyles.reverse : lineStyles.noReverse
+  const reverseOtherClass = isReverse ? "reverse" : "noReverse"
+  const [ref, inView, entry] = useInView({
+    /* Optional options */
+    threshold: 0,
+  })
   return (
-    <div className={`${lineStyles.container} ${reverseClass}`}>
+    <div
+      ref={ref}
+      className={`${lineStyles.container} ${
+        inView ? lineStyles.animate : lineStyles.noAnimate
+      } reverseOtherClass ${reverseClass}`}
+    >
       <div className={`${lineStyles.lineContainer}`}>
-        <div className={`${lineStyles.line} ${lineStyles.lineBig}`}></div>
-        <div className={`${lineStyles.line} ${lineStyles.lineMedium}`}></div>
-        <div className={`${lineStyles.line} ${lineStyles.lineSmall}`}></div>
+        <div
+          className={`${lineStyles.line}  lineBig ${lineStyles.lineBig}`}
+        ></div>
+        <div
+          className={`${lineStyles.line} lineMedium ${lineStyles.lineMedium}`}
+        ></div>
+        <div
+          className={`${lineStyles.line} lineSmall ${lineStyles.lineSmall}`}
+        ></div>
       </div>
       <div className={`${lineStyles.roundContainer}`}>
-        <div className={`${lineStyles.round} ${lineStyles.roundBig}`}></div>
-        <div className={`${lineStyles.round} ${lineStyles.roundSmall}`}></div>
+        <div
+          className={`${lineStyles.round} roundBig ${lineStyles.roundBig}`}
+        ></div>
+        <div
+          className={`${lineStyles.round} roundSmall ${lineStyles.roundSmall}`}
+        ></div>
       </div>
     </div>
   )
