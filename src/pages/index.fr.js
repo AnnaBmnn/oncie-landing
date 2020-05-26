da55da3dce7bae3ad335eb1e7fba2cf42c75543a
Anna <annabaumann@laposte.net>
2020-05-26 12:41:35 +0200

    change .ecriture inclusive

--- src/pages/index.fr.js
+++ src/pages/index.fr.js
@@ -35,7 +35,7 @@ const IndexFrenchPage = ({ location }) => {
       <Section>
         <SectionHeader
           width="big"
-          title="Oncie permet à tous les podcasteur•ices d’offrir à leur communauté une nouvelle expérience audio "
+          title="Oncie permet à tous les podcasteur·ices d’offrir à leur communauté une nouvelle expérience audio "
           titleHighlight=" en live"
         />
         <ImgFullWidth
@@ -44,8 +44,8 @@ const IndexFrenchPage = ({ location }) => {
         ></ImgFullWidth>
         <BlocImgText
           imgSrc={img}
-          title="Vos auditeur•ices sont juste de l’autre côté du micro"
-          description="En proposant un live sur Oncie, vous offrez l’opportunité à votre communauté d’accéder à du contenu exclusif, d’échanger, de réagir, de poser des questions, voire même de prendre la parole et d’être acteur•ices dans leur consommation. Mais surtout, de se sentir part entière d’une communauté qui leur ressemble et vous rassemble."
+          title="Vos auditeur·ices sont juste de l’autre côté du micro"
+          description="En proposant un live sur Oncie, vous offrez l’opportunité à votre communauté d’accéder à du contenu exclusif, d’échanger, de réagir, de poser des questions, voire même de prendre la parole et d’être acteur·ices dans leur consommation. Mais surtout, de se sentir part entière d’une communauté qui leur ressemble et vous rassemble."
         />
       </Section>
       <BlocPhone
@@ -64,7 +64,7 @@ const IndexFrenchPage = ({ location }) => {
           title1="La meilleure façon d’interagir avec votre communauté en temps réel"
           description1="Oncie vous permet de recevoir les questions et commentaires les plus pertinents via un système de vote. Sondages, réactions, partage d’émotions : le live chat laisse la place aux messages de votre communauté en temps réel. "
           title2="L’opportunité d’animer des émission à plusieurs "
-          description2="Avez-vous déjà pensé à inviter une personne lors d’une émission ? Planifier une interview à distance ou diffuser un témoignage spontané d’une de vos auditeur•ices est desormais possible grâce à la fonctionnalité mutli-live. "
+          description2="Avez-vous déjà pensé à inviter une personne lors d’une émission ? Planifier une interview à distance ou diffuser un témoignage spontané d’une de vos auditeur·ices est desormais possible grâce à la fonctionnalité mutli-live. "
           title3="Faites grandir votre émission, tout en détail"
           description3="Personnalisez et caractérisez vos émissions en ajoutant jingles et bandes son. Accédez aux données relatives à votre activité (nombre d’écoutes, interactions) pour comprendre votre audimat et optimiser l’impact de vos interventions."
         ></Features>
@@ -81,7 +81,7 @@ const IndexFrenchPage = ({ location }) => {
         <SectionHeader
           width="big"
           title="Vous souhaitez tester en exclusivité la bêta ?"
-          description="Entrez votre email et soyez le•a premier•e à utiliser Oncie avant sa sortie publique."
+          description="Entrez votre email et soyez le·a premier·e à utiliser Oncie avant sa sortie publique."
         />
         <CallToAction
           placeHolderTxt="Entrez votre email"
